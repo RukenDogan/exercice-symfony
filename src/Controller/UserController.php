956b0460d61106e34538ca8e7008b8eb6433f208
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-01-13 17:26:07 +0100

    display users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,8 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
@@ -19,4 +21,22 @@ class UserController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    #[Route('/users/add-test', name: 'user_add_test')]
+    public function addTest(EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $user->setId(1);
+        $user->setNom('Snoop');
+        $user->setPrenom('Dogg');
+        $user->setEmail('snoop.dogg@test.com');
+        $user->setAdresse('12 rue de la gare');
+        $user->setTel('06 00 00 00 00');
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('user_list');
+    }
+
 }
